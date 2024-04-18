9ac488cb723d5dfb450a4e21a8184a0f00ded96a
André Ribeiro <49374221+Arvaesri@users.noreply.github.com>
2024-04-17 22:27:52 -0300

    feat: create base TodoItemService and ITodoItemService

---
+++ TodoListApi/Interfaces/ITodoItemService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoListApi.Models;
+
+namespace TodoListApi.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem> CreateTodoItemAsync(string title, string description, DateTime? dueDate);
+        Task<IEnumerable<TodoItem>> GetAllTodoItemsAsync();
+        Task<TodoItem> GetTodoItemByIdAsync(Guid id);
+        Task UpdateTodoItemAsync(Guid id, string title, string description, bool completed, DateTime updated, DateTime? dueDate);
+        Task DeleteTodoItemAsync(Guid id);
+    }
+}
---
+++ TodoListApi/Services/TodoItemService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoListApi.Interfaces;
+
+namespace TodoListApi.Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        public async Task<TodoItem> CreateTodoItemAsync(string title, string description, DateTime? dueDate)
+        {
+            var todoItem = new TodoItem(title, description, dueDate);
+            // You might add data access logic here to save the todoItem to a database
+            return await Task.FromResult(todoItem);
+        }
+
+        public async Task<IEnumerable<TodoItem>> GetAllTodoItemsAsync()
+        {
+            // You might add data access logic here to retrieve all todoItems from a database
+            return await Task.FromResult(new List<TodoItem>());
+        }
+
+        public async Task<TodoItem> GetTodoItemByIdAsync(Guid id)
+        {
+            // You might add data access logic here to retrieve a todoItem by its id from a database
+            return await Task.FromResult<TodoItem>(null);
+        }
+
+        public async Task UpdateTodoItemAsync(Guid id, string title, string description, bool completed, DateTime updated, DateTime? dueDate)
+        {
+            // You might add data access logic here to update a todoItem in a database
+            await Task.CompletedTask;
+        }
+
+        public async Task DeleteTodoItemAsync(Guid id)
+        {
+            // You might add data access logic here to delete a todoItem from a database
+            await Task.CompletedTask;
+        }
+    }
+}
