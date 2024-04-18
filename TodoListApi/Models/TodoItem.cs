be57c426400a856c4924b294e789d608198bf7e9
André Ribeiro <49374221+Arvaesri@users.noreply.github.com>
2024-04-17 22:25:55 -0300

    feat: add model for todoItem

---
+++ TodoListApi/Models/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace TodoListApi.Models
+{
+    internal class TodoItem
+    {
+        public Guid Id {  get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public bool Completed { get; private set; }
+        public readonly DateTime created; 
+        public DateTime Updated { get; private set; }
+        public DateTime? dueDate;
+        public TodoItem(string title, string description, DateTime? dueDate)
+        {
+            this.Id = Guid.NewGuid();
+            this.Title = title;
+            this.Description = description;
+            this.Completed = false;
+            this.created = DateTime.Now;
+            this.dueDate = dueDate;
+        }
+    }
+}
