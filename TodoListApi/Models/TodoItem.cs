01b3c456ea1db9ea4ebd0215455df0c3a9d448d5
André Ribeiro <49374221+Arvaesri@users.noreply.github.com>
2024-04-19 23:20:33 -0300

    chore: update model class properties

--- TodoListApi/Models/TodoItem.cs
+++ TodoListApi/Models/TodoItem.cs
@@ -5,11 +5,12 @@ namespace TodoListApi.Models
     public class TodoItem
     {
         public Guid Id {  get; private set; } = Guid.NewGuid();
-        public string Title { get; private set; } = string.Empty;
-        public string Description { get; private set; } = string.Empty;
-        public bool Completed { get; private set; } = false;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool Completed { get; set; } = false;
         public readonly DateTime created = DateTime.Now;
-        public DateTime Updated { get; private set; } = DateTime.Now;
-        public DateTime? dueDate { get; private set; } = null;
+        public DateTime Updated { get; set; } = DateTime.Now;
+        public DateTime? DueDate { get; set; } = null;
+        public bool Deleted { get; set; } = false;
     }
 }
